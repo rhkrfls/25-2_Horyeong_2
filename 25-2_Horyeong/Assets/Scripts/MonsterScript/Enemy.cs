3c4abcda2870053327464b265ffbffb749a16f39
diomat0980 <109652716+diomat0980@users.noreply.github.com>
2025-09-26 01:20:30 +0900

    monster_3day

--- 25-2_Horyeong/Assets/Scripts/MonsterScript/Enemy.cs
+++ 25-2_Horyeong/Assets/Scripts/MonsterScript/Enemy.cs
@@ -2,29 +2,90 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 5f;
+    public enum monster_attack_type
+    {
+        MELEE,
+        MID_RANGED,
+        RANGED
+    };
+
+    public enum monster_id
+    {
+        STRAYDOG,
+        CROW,
+        PINE,
+        DANDELION
+    };
+
+    public enum race
+    {
+        HUMAN,
+        ANIMAL,
+        PLANT
+    };
+
+    [Header("# Monster_Enum_type")]
+    public monster_attack_type Attack_enumType;
+    public monster_id Id_enumType;
+    public race Race_enumType;
+
+    [Header("# Monster_Int")]
+    public int monster_damage = 1;
+    public int monster_hp = 1;
+    public int monster_maxHp = 1;
+
+    [Header("# Monster_Range")]
+    public float monster_sight_range = 0f;
+    public float monster_attack_range = 0f;
+    public float monster_chase_ranges = 0f;
+
+    [Header("# Monster_Speed")]
+    public float monster_speed = 5f;
+    public float monster_rotationSpeed = 5f;
+
+    [Header("# Monster_Bool")]
+    public bool monster_detected = false;
+    public bool monster_attacking = false;
+    public bool monster_chasing = false;
+
+
+    [Header("# Monster_LayerMask")]
+    public LayerMask layer;
 
     private Transform player;
-
-    public float radius = 0f;
-
-    public LayerMask layer;
 
     public Collider2D[] coll;
     public Collider2D Short_player;
     private SpriteRenderer spriteRenderer;
 
 
-    void Start()
+    private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    private void OnEnable()
     {
-        coll = Physics2D.OverlapCircleAll((Vector2)transform.position, radius, layer);
+        foreach(Collider2D collider2D in coll)
+        {
+            if(collider2D != null)
+            {
+                collider2D.enabled = true;
+            }
+        }
+        monster_hp = monster_maxHp;
+    }
+
+    private void Update()
+    {
+        MonsterSightRange();
+        MonsterAttack();
+    }
+
+    private void MonsterSightRange()
+    {
+        coll = Physics2D.OverlapCircleAll((Vector2)transform.position, monster_sight_range, layer);
 
         if (coll.Length > 0)
         {
@@ -49,7 +110,7 @@ public class Enemy : MonoBehaviour
                 direction.Normalize();
 
                 // 이동 (X축만 이동)
-                transform.position += new Vector3(direction.x, 0, 0) * speed * Time.deltaTime;
+                transform.position += new Vector3(direction.x, 0, 0) * monster_speed * Time.deltaTime;
 
                 // 좌우 반전 (플레이어 위치에 따라 flip)
                 if (direction.x < 0)
@@ -57,6 +118,37 @@ public class Enemy : MonoBehaviour
                 else if (direction.x > 0)
                     spriteRenderer.flipX = true;  // 오른쪽 바라봄
             }
+        }
+    }
+
+    private void MonsterAttack()
+    {
+        coll = Physics2D.OverlapCircleAll((Vector2)transform.position, monster_attack_range, layer);
+
+        if (coll.Length > 0)
+        {
+            float short_distance = Vector3.Distance(transform.position, coll[0].transform.position);
+
+            foreach (Collider2D col in coll)
+            {
+                if (col == null) continue;
+
+                float short_distance2 = Vector3.Distance(transform.position, col.transform.position);
+                if (short_distance > short_distance2)
+                {
+                    short_distance = short_distance2;
+                    Short_player = col;
+                }
+            }
+
+            if (Short_player != null)
+            {
+                monster_attacking = true;
+            }
+        }
+        else
+        {
+            monster_attacking = false;
         }
     }
 
@@ -68,6 +160,10 @@ public class Enemy : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(transform.position, monster_sight_range);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, monster_attack_range);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, monster_chase_ranges);
     }
 }
